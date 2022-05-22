9bf4bc10d410f255b72afb6f6ec0f172ea3449fc
Ashton Wiersdorf <ashton.wiersdorf@pobox.com>
2022-05-21 23:57:24 -0600

    Add playground

---
+++ relations_playground.rkt
@@ -0,0 +1,23 @@
+#lang racket/base
+
+(require "kanren.rkt")
+
+(define (parent c p)
+  (conde
+   [(== c 'teddy) (== p 'saraih)]
+   [(== c 'andrew) (== p 'steve)]
+   [(== c 'steve) (== p 'bill)]
+   [(== c 'anne) (== p 'john)]
+   [(fresh (s) (married p s) (parent c s))]
+   [(fresh (s) (married s p) (parent c s))]))
+
+(define (married h w)
+  (conde
+   [(== h 'steve) (== w 'anne)]
+   [(== h 'bill) (== w 'katie)]
+   [(== h 'john) (== w 'kitty)]
+   [(== h 'andrew) (== w 'saraih)]))
+
+(define (grandparent g s)
+  (fresh (p) (parent g p) (parent p s)))
+
