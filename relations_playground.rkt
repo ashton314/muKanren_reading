e1382963fceef4f5f7b2da451f10e23d2f1c5d50
Ashton Wiersdorf <ashton.wiersdorf@pobox.com>
2022-06-12 16:03:03 -0600

    Add README builder; more examples in relations playground

--- relations_playground.rkt
+++ relations_playground.rkt
@@ -6,14 +6,24 @@
   (conde
    [(== c 'teddy) (== p 'saraih)]
    [(== c 'andrew) (== p 'steve)]
+   [(== c 'brook) (== p 'steve)]
+   [(== c 'caroline) (== p 'steve)]
+   [(== c 'peter) (== p 'steve)]
    [(== c 'steve) (== p 'bill)]
+   [(== c 'roger) (== p 'bill)]
+   [(== c 'will) (== p 'roger)]
+   [(== c 'andy) (== p 'roger)]
+   [(== c 'earnest) (== p 'roger)]
+   [(== c 'jack) (== p 'bill)]
    [(== c 'anne) (== p 'john)]
+   [(== c 'danni) (== p 'john)]
    [(fresh (s) (married p s) (parent c s))]
    [(fresh (s) (married s p) (parent c s))]))
 
 (define (married h w)
   (conde
    [(== h 'steve) (== w 'anne)]
+   [(== h 'nate) (== w 'danni)]
    [(== h 'bill) (== w 'katie)]
    [(== h 'john) (== w 'kitty)]
    [(== h 'andrew) (== w 'saraih)]))
@@ -22,6 +32,11 @@
   (fresh (p) (parent g p) (parent p s)))
 
 ;; (run 20 (rel p c) (conj+ (grandparent c p) (== (cons c p) rel)))
+;; (list->set (run 10 (gp) (grandparent 'andrew gp)))
+
+;; unfortunately we need to gather 93 results until we get all the
+;; answers for bill's grandchildren:
+;; (list->set (run 93 (c) (grandparent c 'bill)))
 
 (define (fav-num n)
   (disj (== n 42)
